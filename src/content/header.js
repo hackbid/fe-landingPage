b73d7d2b87f49529f2858c478368421fa4d15d8f
mashayyik <mashayyik@gmail.com>
2023-03-09 17:30:27 +0700

    refactor navbar

--- src/content/header.js
+++ src/content/header.js
@@ -1,25 +1,36 @@
-import imageLogo from "../assets/logoimage.png";
+import { useState } from 'react';
+import imageLogo from '../assets/logoimage.png';
 const Header = () => {
-  return (
-    <div>
-      <header
-        className="flex justify-start gap-48 px-5 py-3 max-w-5xl m-auto items-center"
-        style={{ color: "#302F2E" }}
-      >
-        <img src={imageLogo} className="h-24 opacity-90" />
-        <ul className="flex gap-14 font-medium text-xl">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About Us</a>
-          </li>
-          <li>
-            <a href="#testmonial">Developers</a>
-          </li>
-        </ul>
-      </header>
-    </div>
-  );
+    const [activeNavbar, setActiveNavbar] = useState(false);
+    const changeBackground = () => {
+        if (window.scrollY >= 100) {
+            setActiveNavbar(true);
+        } else {
+            setActiveNavbar(false);
+        }
+    };
+    window.addEventListener('scroll', changeBackground);
+    return (
+        <div>
+            <header
+                className={
+                    (activeNavbar ? 'bg-white border-b-4 ' : 'bg-transparent') +
+                    ' flex fixed w-full mx-auto justify-center  z-50  gap-48 px-5 py-1  m-auto items-center'
+                }>
+                <img src={imageLogo} className='h-24 opacity-90' />
+                <ul className='flex gap-14 font-medium text-xl'>
+                    <li>
+                        <a href='#home'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#about'>About Us</a>
+                    </li>
+                    <li>
+                        <a href='#testmonial'>Developers</a>
+                    </li>
+                </ul>
+            </header>
+        </div>
+    );
 };
 export default Header;
